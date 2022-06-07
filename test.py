d4a2dbc60cad2f7749b1a896c4f2dac83e95b2e8
Vladi <vladibuyanov@gmail.com>
2022-06-07 15:03:58 +0200

    07/06

--- test.py
+++ test.py
@@ -8,17 +8,4 @@ user_id = 1
 id_list = [i['id'] for i in res]
 print(id_list)
 
-# for i in res:
-#     id_list.append(i['id'])
-#
-#
-# if user_id in id_list:
-#     print(True)
-#     for i in res:
-#         print(i)
-# else:
-#     print(False)
-
-
-# print(res.json())
 
