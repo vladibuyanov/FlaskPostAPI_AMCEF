bed829c8a6d8ce5f9154d296f4c7f98e81a2c849
vladibuyanov <vladibuyanov@gmail.com>
2022-06-19 16:48:30 +0200

    Ver. 19/06

--- Backend/tests/test_validation_title_body.py
+++ Backend/tests/test_validation_title_body.py
@@ -3,8 +3,11 @@ from Backend.function import validation_title_body
 
 
 @pytest.mark.parametrize('title, body', [('First post', "It's first post")])
-def test_validation_title_body(title, body):
+def test_validation_title_body_positive(title, body):
     assert validation_title_body(title, body)
 
 
-# @pytest.mark.parametrize('title, body, error', [('First post', "It's first post")])
+@pytest.mark.parametrize('title, body, error', [(1, "It's first post", AssertionError)])
+def test_validation_title_body_negative(title, body, error):
+    with pytest.raises(error):
+        assert validation_title_body(title, body)
